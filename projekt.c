e37a81e8bfddcc8d25076aa7ccd92ce0d307ef81
Jakub Tomczak <jakub.tomczak@live.com>
2018-05-09 14:52:31 +0200

    Jedziemy z kolejka.

--- projekt.c
+++ projekt.c
@@ -3,9 +3,19 @@
 #include<pthread.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include "stdafx.h"
 #define P 5
 #define J 20
 #define L 10
+
+/*
+	status
+		transport do szpitala: 0
+		szpital: 1
+		transport ze szpitala: 2
+
+*/
+
 int status;
 queue *pocz,*s;
 struct queue
@@ -34,13 +44,16 @@ else
 
 int pop()
 {
-int item;
+int item = -1;
 if(pocz!=NULL)
 	{
 		s=pocz;
 		item=pocz->id;
 		pocz=s->next;
-		
+		free(s);
+	}
+	return item;
+}
 		
 
 		
@@ -90,17 +103,18 @@ int main( int argc, char **argv )
 	status=0;
 	sleep(1,10);
 	status=1;
+		
+	struct queue transport_hospital_queue;
 	
+	transport_hospital_queue.push(1);
 
+	transport_hospital_queue.push(1);
+	transport_hospital_queue.push(2);
+	transport_hospital_queue.push(3);
 
-
-
-
-
-
-
-
-
-
+	for(int i = 0 ; i< 10;i++){
+		printf("indeks %d, wartosc %d\n", i, transport_hospital_queue.pop());
+	}
 	MPI_Finalize();
 	
+}
