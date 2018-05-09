a4e75a151f434ef8d5659a998b87795e0d63967d
Student inf127331 <inf127331@lab-sec-18.cs.put.poznan.pl>
2018-05-09 13:03:35 +0200

    init

---
+++ projekt.c
@@ -0,0 +1,81 @@
+#include "mpi.h"
+#include <stdio.h>
+#include<pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define P 5
+#define J 20
+#define L 10
+int status;
+queue *pocz,*s,*z;
+struct queue
+{
+    int id;
+    queue *next;
+};
+
+void push(int id)
+{
+if(pocz==NULL)
+	{
+		s=new queue;
+
+
+struct sectionRequest{
+    int clock;
+    int section;
+}
+
+struct sectionResponse{
+    int status;
+    int section;
+}
+
+void initParallelThread()
+{
+	pthread_t pass_thread;
+	pthread_create(&pass_thread, NULL, communicationThread, NULL);
+}
+
+void communicationThread()
+{
+	while(1)
+	{
+	MPI_Recv(msg, MSG_SIZE, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+	}
+}
+
+int random(int min, int max)
+{
+    int tmp;
+    if (max>=min)
+        max-= min;
+    else
+    {
+        tmp= min - max;
+        min= max;
+        max= tmp;
+    }
+    return max ? (rand() % max + min) : min;
+}
+
+int main( int argc, char **argv )
+{
+	MPI_Init(&argc, &argv);
+	status=0;
+	sleep(1,10);
+	status=1;
+	
+
+
+
+
+
+
+
+
+
+
+
+	MPI_Finalize();
+	
